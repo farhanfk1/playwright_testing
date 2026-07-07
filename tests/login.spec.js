3ccd082504d603ec653e890016e638d6a87eb6d4
farhanfk1 <farhankhanbamkhel170@gmail.com>
2026-07-07 10:45:56 +0500

    import chamge

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,6 +1,6 @@
 const { test, expect } = require("@playwright/test");
 
-const LoginPage = require("../pages/LoginPage");
+const LoginPage = require("../pages/loginPage");
 
 const data = require("../test-data/loginData");
 
